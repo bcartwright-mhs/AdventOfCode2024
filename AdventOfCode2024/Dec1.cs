3dc4fb81fdfb390f514c8b8b7f775a141a142f6e
Brandon Cartwright <bcartwright@memorialcare.org>
2024-12-02 08:34:29 -0800

    Initial commit

---
+++ AdventOfCode2024/Dec1.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode2024;
+
+public static class Dec1
+{
+    public static int ProcessFirstChallenge(string dataFileName, string dataSeperator, int expectedItemCount)
+    {
+        if (!File.Exists(dataFileName))
+        {
+            Console.WriteLine("File not found: " + dataFileName);
+            return -1;
+        }
+        
+        var firstList = new List<int>();
+        var secondList = new List<int>();
+
+        using (var reader = new StreamReader(dataFileName))
+        {
+            try
+            {
+                int lineNumber = 0;
+                while (!reader.EndOfStream)
+                {
+                    lineNumber++;
+                    var line = reader.ReadLine();
+
+                    if (line != null && line.Length > 1)
+                    {
+                        // Parse the file into 2 lists
+                        try
+                        {
+                            var items = line.Split(dataSeperator);
+                            if (items.Length == expectedItemCount) 
+                            {
+                                int int1 = 0;
+                                int int2 = 0;
+
+                                if (!int.TryParse(items[0], out int1)) 
+                                {
+                                    Console.WriteLine($"Error converting {items[0]} to int on line number {lineNumber}");
+                                    return -1;
+                                }
+                                if (!int.TryParse(items[1], out int2)) 
+                                {
+                                    Console.WriteLine($"Error converting {items[1]} to int on line number {lineNumber}");
+                                    return -1;
+                                }
+                                firstList.Add(int1);
+                                secondList.Add(int2);
+                            }
+                           
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error processing file: " + ex.Message);
+                        }
+                    }
+                }
+
+                // Sort the Lists
+                firstList.Sort();
+                secondList.Sort();
+
+                // Loop through and find the diff
+                int totalDiff = 0;
+                for(int i=0;i<firstList.Count;i++)
+                {
+                    totalDiff += Math.Abs(firstList[i] - secondList[i]);
+                    if (firstList[i] < secondList[i])
+                    {
+                        Console.WriteLine($"First is less than second on line {i}");
+                        
+                    }
+                }
+                return totalDiff;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error reading from file: " + ex.Message);
+                return -1;
+            }
+        }
+    }
+
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.CommandLine;
+using System.Reflection;
+using AdventOfCode2024;
+
+Console.Clear();
+Console.WriteLine("Welcome to the Advent of Code 2024 Tool.");
+Console.WriteLine("Type 'h' for a list of available commands.");
+string prompt = "AOC2024>";
+//DateTime utcDateTime;
+
+Console.Write(string.Format(prompt, 0));
+string? input = Console.ReadLine();
+while (input != null && input.Length > 0 && input.ToLower() != "quit" && input.ToLower() != "q")
+{
+    string[] input_parts = input.Split(' ');
+    string command = input_parts[0].ToLower();
+
+    switch (command)
+    {
+        case "puzzle_1a":
+        case "p1a":
+            if (input_parts.Length == 2) 
+            {
+                var output = Dec1.ProcessFirstChallenge(input_parts[1], "   ", 2);
+                if (output > 0)
+                {
+                    Console.WriteLine(output);
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Missing input file name");
+            }
+            break;
+        case "clear":
+        case "cls":
+            Console.Clear();
+            break;
+        case "version":
+        case "v":
+            Console.WriteLine("Version info not implemented yet!");
+            break;
+        case "help":
+        case "h":
+            Console.WriteLine("Commands:");
+            Console.WriteLine("  puzzle_1a (p1a) <filename> - Processes file for the first puzzle on Dec 1.");
+            Console.WriteLine("  clear (cls)");
+            Console.WriteLine("  version (v)");
+            Console.WriteLine("  quit (q)");
+            break;
+        default:
+            Console.WriteLine("Unknown command: " + input);
+            break;
+    }
+
+    Console.WriteLine("");
+    Console.Write(prompt);
+    input = Console.ReadLine();
+}
+
